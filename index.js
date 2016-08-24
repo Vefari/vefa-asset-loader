a8f269b8077cb4d3869b8ce6126efecf47522609
Cobey Potter <cobey.potter@wellfireinteractive.com>
2016-08-24 18:54:23 -0400

    tighten up code

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-var loader_utils = require('loader-utils');
+var utils = require('loader-utils');
 
 module.exports = function(source){
     this.cacheable && this.cacheable();
-
-    // get the options contained in the query
-    var query = loader_utils.parseQuery(this.query);
     
-    // get the extra options not contained in the query
-    var opts = this.options.vefaAssets || {};
+    // get config
+    var config = utils.getLoaderConfig(this, "vefaAsset");
     
     // get the specific context involved, defaulting to Webpack's declared context
     var context = {
-        context: query.context || opts.context || this.options.context,
+        context: config.context || this.options.context,
         content: source
     };
     
     // define a specific extension either through the query or through the options
     // if no extension given, default to the one sent aong
-    var extension = query.extension || opts.extension || "[ext]";
+    var extension = config.extension || "[ext]";
 
     // create the general basic filepath
     var default_path = "[path][name]." + extension;
 
-    var file_path = query.export || opts.export || default_path;
+    var file_path = config.export || default_path;
     
     // lets sub out all the placeholders
-    var file = loader_utils.interpolateName(
+    var file = utils.interpolateName(
         this,
         file_path,
         context
